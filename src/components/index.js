362d8bbd2bc8211974222c5f30a72282bc16fe70
Ammar-Amin <ammaramin5253@gmail.com>
2024-06-11 10:55:14 +0530

    add Login Components using react-hook-form

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,8 @@ import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 import PostCard from "./PostCard";
+import Login from "./Login";
 
 export {
-    Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, PostCard,
+    Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, PostCard, Login,
 }
