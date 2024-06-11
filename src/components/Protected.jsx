107ceed6d86c14c286e942c5f8d3a663f5091821
Ammar-Amin <ammaramin5253@gmail.com>
2024-06-11 13:39:16 +0530

    add Protected component

---
+++ src/components/Protected.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+export default function Protected({
+    children,
+    authenticated = true,
+}) {
+
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        // we know this 
+        authStatus === false ? navigate('/login') : navigate('/')
+
+        // this is advance stuff 
+        if (authenticated && authStatus !== authenticated) {
+            // true && false !== true 
+            navigate('/login')
+        }
+        else if (!authenticated && authStatus !== authenticated) {
+            // false && false !== true 
+            navigate('/')
+        }
+        setLoading(false)
+    }, [navigate, authStatus, authenticated])
+
+    return loading
+        ? <h1 className='text-slate-200 text-3xl text-center'>Loading...</h1>
+        : <>{children}</>
+}
--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,9 @@ import Input from "./Input";
 import Select from "./Select";
 import PostCard from "./PostCard";
 import Login from "./Login";
+import Signup from "./Signup";
+import Protected from "./Protected";
 
 export {
-    Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, PostCard, Login,
+    Header, Footer, Container, Logo, LogoutBtn, Button, Input, Select, PostCard, Login, Signup, Protected,
 }
